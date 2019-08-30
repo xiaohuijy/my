381772463294f231e6cc1064748525ff78f99ee8
xiaohuijy <50646340+xiaohuijy@users.noreply.github.com>
2019-08-30 04:04:07 -0500

    Create voc.py

---
+++ voc.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  9 08:57:37 2019
+
+@author: 10264228@zte.intra
+"""
+
+import os
+import random
+
+trainval_percent = 0.1
+train_percent = 0.9
+xmlfilepath = 'Annotations'
+txtsavepath = 'ImageSets\Main'
+total_xml = os.listdir(xmlfilepath)
+
+num = len(total_xml)
+list = range(num)
+tv = int(num * trainval_percent)
+tr = int(tv * train_percent)
+trainval = random.sample(list, tv)
+train = random.sample(trainval, tr)
+
+ftrainval = open('ImageSets/Main/trainval.txt', 'w')
+ftest = open('ImageSets/Main/test.txt', 'w')
+ftrain = open('ImageSets/Main/train.txt', 'w')
+fval = open('ImageSets/Main/val.txt', 'w')
+
+for i in list:
+    name = total_xml[i][:-4] + '\n'
+    if i in trainval:
+        ftrainval.write(name)
+        if i in train:
+            ftest.write(name)
+        else:
+            fval.write(name)
+    else:
+        ftrain.write(name)
+
+ftrainval.close()
+ftrain.close()
+fval.close()
+ftest.close()
